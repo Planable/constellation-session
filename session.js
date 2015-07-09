5e121566bd187e540d7d4cf668b768cb31e8e516
Brent Abrahams <brent_abrahams@yahoo.com>
2015-07-09 10:43:18 +0800

    Support for Object.observe instead of just polling and increasing efficiency while polling

--- session.js
+++ session.js
@@ -11,13 +11,41 @@ Constellation.addTab({
 var ReactiveDictDep = new Tracker.Dependency;
 var currentDict = new ReactiveVar({
   name: "Session",
-  dict:Session
+  dict: Session
 });
 
-// Check if someone has added something to the Session every 3 seconds
-Meteor.setInterval(function () {
-  ReactiveDictDep.changed();
-}, 3000);
+if (Object.observe) {
+  Tracker.autorun(function () {
+    var cd = currentDict.get();
+    Object.observe(cd.dict.keys, function (changes) {
+	  // This does a double redraw of the editableJSON for Session, because the editableJSON is stored in the Session
+      ReactiveDictDep.changed();
+    });
+  });
+}
+else {
+  // Check if someone has added something to the Session every 3 seconds
+  Meteor.setInterval(function () {
+    var currentTab = Constellation.getCurrentTab();
+    if (currentTab && currentTab.id === 'Session') {
+      // This could get heavy for large reactive dictionaries, so we're going for every 3 seconds
+      var change = false;
+      var siftedDict = {};
+      var currentJSON = EditableJSON.retrieve('constellation_session');
+      _.each(currentDict.get().dict.keys, function (val, key) {
+        if ((key.indexOf('Constellation_') > -1) || (key.indexOf('Meteor') > -1) || (key.indexOf('Temple_') > -1) || key === "Constellation" || key === 'editableJSON' || _.isUndefined(val) || val === "undefined") {
+          return;  
+        }
+        if (_.isUndefined(currentJSON[key]) || currentJSON[key] !== EJSON.parse(val)) {
+          change = true;
+        }
+      });
+      if (change) {
+        ReactiveDictDep.changed();
+      }
+    }
+  }, 3000);
+}
 
 EditableJSON.afterUpdate(function (store, action, JSONbefore, documentsUpdated) {
   // Make the changes
@@ -45,7 +73,7 @@ Template.Constellation_session_main.helpers({
         return;  
       }
       siftedDict[key] = EJSON.parse(val);
-    });
+    }); console.log('redraw');
     return siftedDict;
   }
 });
