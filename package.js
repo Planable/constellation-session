401eff0ab6121aebc6c86c5a5f91052378fff1ce
Brent Abrahams <brent_abrahams@yahoo.com>
2018-02-20 08:36:05 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'constellation:session',
-  version: '0.4.5',
+  version: '0.4.6',
   summary: 'Session variable editor plugin for Constellation',
   git: 'https://github.com/JackAdams/constellation-session.git',
   documentation: 'README.md',
@@ -11,8 +11,8 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
   api.use(['templating','session','blaze','underscore','ejson','tracker','reactive-var','reactive-dict'], 'client');
-  api.use('constellation:console@1.4.4', 'client');
-  api.use('babrahams:editable-json@0.6.4', 'client');
+  api.use('constellation:console@1.4.6', 'client');
+  api.use('babrahams:editable-json@0.6.5', 'client');
 
   api.addFiles('session.css','client');
   api.addFiles('session.html','client');
